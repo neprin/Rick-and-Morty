950c4962fbcaac91ea9e5814a5f937823c2ae81a
Pavel Neprin <41507524+neprin@users.noreply.github.com>
2022-12-04 14:02:23 +0300

    Version 1.0 / made icon, Splash screen

---
+++ Rick and Morty UIKit/Favourites/FavouritesViewController.swift
@@ -0,0 +1,8 @@
+//
+//  FavouritesViewController.swift
+//  Rick and Morty UIKit
+//
+//  Created by Pavel Neprin on 12/4/22.
+//
+
+import Foundation
---
+++ Rick and Morty UIKit/MainTabBarController.swift
@@ -0,0 +1,8 @@
+//
+//  MainTabBarController.swift
+//  Rick and Morty UIKit
+//
+//  Created by Pavel Neprin on 12/4/22.
+//
+
+import Foundation
