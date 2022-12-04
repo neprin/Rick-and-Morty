8826e216313b0421586dff9301886937dc93bca8
Pavel Neprin <41507524+neprin@users.noreply.github.com>
2022-12-04 16:03:55 +0300

    section all characters

--- Rick and Morty UIKit/Characters/CharactersViewController.swift
+++ Rick and Morty UIKit/Characters/CharactersViewController.swift
@@ -9,4 +9,78 @@ import UIKit
 
 class CharactersViewController: UICollectionViewController {
     
+    // две кнопки для функционала в tab bar
+    private lazy var addBarButtonItem: UIBarButtonItem = {
+        return UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addBarButton))
+    }()
+    
+    private lazy var shareBarButtonItem: UIBarButtonItem = {
+        return UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareBarButton))
+    }()
+    
+    //MARK: - viewDidLoad()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .systemBackground
+        setupCollectionView()
+        setupNavigationBar()
+        setupSearchBar()
+    }
+    
+    //MARK: - Navigation Items (Buttons)
+    
+    @objc private func addBarButton() {
+        print(#function)
+    }
+    
+    @objc private func shareBarButton() {
+        print(#function)
+    }
+    
+    //MARK: - Setup UI Elements
+    
+    private func setupCollectionView() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellID")
+    }
+    
+    // настраваем navigation bar
+    private func setupNavigationBar() {
+        let titleLabel = UILabel()
+        titleLabel.text = "All characters"
+        titleLabel.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: titleLabel)
+        navigationItem.rightBarButtonItems = [shareBarButtonItem, addBarButtonItem]
+    }
+    
+    // настраивакм search bar
+    func setupSearchBar() {
+        let searchController = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        // чтобы сработал extension delegate
+        searchController.searchBar.delegate = self
+    }
+    
+    //MARK: - UICollectionViewDataSource, UICollectionViewDelegate
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    // метод отвечает за настройку конкретной ячейки
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath)
+        cell.backgroundColor = .orange // временно
+        return cell
+    }
+}
+
+//MARK: - UISearchBarDelegate Extension
+
+extension CharactersViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+    }
 }
--- Rick and Morty UIKit/SceneDelegate.swift
+++ Rick and Morty UIKit/SceneDelegate.swift
@@ -18,6 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
         
+        // без кода ниже черный экран
         window = UIWindow()
         window?.makeKeyAndVisible()
         window?.rootViewController = MainTabBarController()
